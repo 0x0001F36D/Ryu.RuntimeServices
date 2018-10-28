b91e4ffc5e525061058e85f2e9164a9bc3b420ff
俞俞 <hanabiranoshitaniumore@gmail.com>
2018-10-28 10:24:26 +0800

    ^

--- test/Ryuko.UnitTest/Geolocation/GeolocationServiceTest.cs
+++ test/Ryuko.UnitTest/Geolocation/GeolocationServiceTest.cs
@@ -8,23 +8,37 @@ namespace Ryuko.UnitTest.Geolocation
 
     using Ryuko.Geolocation;
     using Ryuko.Geolocation.Mapping;
+    using System.Diagnostics;
 
     [TestFixture]
     public class GeolocationServiceTest
     {
+
         [TestCase]
         public void GetInfo()
         {
             var info = Ipapi.Info;
-            Assert.AreEqual(info.Country, "Taiwan");
+            Assert.AreEqual(info.Country, "United States");
         }
 
         [TestCase]
         public void Mapping()
         {
             var info = Ipapi.Info;
-            var taiwan = info.Mapping<Taiwan>();
-            Assert.AreEqual(taiwan.City, "高雄");
+            var us = info.Mapping<UnitedStates>();
+            Debug.WriteLine(us.City);
+            Assert.IsInstanceOf<UnitedStates>(us);
+        }
+
+        public class UnitedStates : SelfMapping<UnitedStates>
+        {
+            public string City { get; private set; }
+
+            protected override UnitedStates MappingTo(IGeolocation geolocation)
+            {
+                this.City = geolocation.RegionName;
+                return this;
+            }
         }
     }
 }
